bf7995828883f3dad24abd4195cc1d330f2cd376
YusriWaiyuesarai <yusrinetwork@gmail.com>
2026-05-13 09:13:08 +0800

    Use HashRouter for frontend routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Login from './pages/Login';
